2fd3155dfda3f495d46f5922de15cd14e93107ab
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2024-04-27 10:46:03 +0000

    changepassword

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="main-stage">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png"
+          className="pass-Style"
+        />
+      </div>
+    </div>
+  )
+}
+export default App
