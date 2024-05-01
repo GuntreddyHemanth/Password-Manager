01fa9a04323391542ab727219b2f912f8ba747ac
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2024-05-01 19:56:27 +0000

    changesontopform

--- src/component/UserInterface/index.js
+++ src/component/UserInterface/index.js
@@ -3,8 +3,32 @@ import {Component} from 'react'
 import './index.css'
 
 class PasswordManger extends Component {
-    
+  state = {
+    itemsList: [],
+    websiteInput: '',
+    username: '',
+    passwordInput: '',
+    searchInput: '',
+  }
+
+  onChangeWebsite = event => {
+    this.setState({websiteInput: event.target.value})
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({passwordInput: event.target.value})
+  }
+
   render() {
+    const {websiteInput, username, passwordInput, itemsList, searchInput} = this.state
+
+    const updatedList = itemsList.filter(each => 
+        each.websiteInput.toLowerCase().includes(searchInput.toLowerCase())
+
     return (
       <div className="bg-container">
         <img
@@ -33,7 +57,55 @@ class PasswordManger extends Component {
                 onChange={this.onChangeWebsite}
               />
             </div>
+            <div className="input-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/password-manager-username-img.png"
+                alt="username"
+                className="input-log"
+              />
+              <input
+                type="text"
+                value={username}
+                placeholder="Enter Username"
+                className="input-item"
+                onChange={this.onChangeUsername}
+              />
+            </div>
+            <div className="input-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/password-manager-password-img.png"
+                alt="password"
+                className="input-log"
+              />
+              <input
+                type="text"
+                value={passwordInput}
+                placeholder="Enter Password"
+                className="input-item"
+                onChange={this.onChangePassword}
+              />
+            </div>
+            <div className="add-button-container">
+              <button type="submit" className="add-button">
+                Add
+              </button>
+            </div>
           </form>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-lg-img.png"
+            alt="password manager"
+            className="password-manager-image"
+          />
+        </div>
+        <div className="bottom-container">
+          <div>
+            <div className="your-password-container">
+              <div className="your-password-text-cont">
+                <h1 className="password-heading">Your Passwords</h1>
+                <p className="count">{count}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
